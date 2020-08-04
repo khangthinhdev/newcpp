565b053e0317e3553e5d2046a451c6692870e270
khangthinh.dev <khangthinh.dev@gmail.com>
2020-08-05 00:42:24 +0700

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	set<int, greater <int> > s1;
+	set<int> s1;
 	int loop;
 	cin >> loop;
 	for (int i = 0; i < loop; i++)
@@ -12,9 +12,13 @@ int main()
 		int x;
 		cin >> x;
 		s1.insert(x);
-	};	
+	};
 
-
+	for(auto it = s1.begin(); it != s1.end(); ++it)
+	{
+		cout << *it << " ";
+	};
+	
 	system("Pause");
 	return 0;
 }
